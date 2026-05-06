39ec151dfa542430b904d27fd981bbad7c2e19cc
Cesar Perez Vivas <cesarperezvivas@MacBook-Air.local>
2026-05-06 18:17:52 +0200

    Add Google Sheets API integration

---
+++ api/sheets.js
@@ -0,0 +1,22 @@
+const { google } = require('googleapis');
+
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  try {
+    const credentials = JSON.parse(process.env.GOOGLE_SHEETS_API_KEY);
+    const auth = new google.auth.GoogleAuth({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: 'Sheet1!A2:J',
+    });
+    res.status(200).json({ data: response.data.values || [] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
